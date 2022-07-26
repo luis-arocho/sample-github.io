5821385209c84e9ab85de9a50e783347c71cf2c9
luis-arocho <94925398+luis-arocho@users.noreply.github.com>
2022-07-26 11:36:02 -0400

    Update testMashup.js

--- testMashup.js
+++ testMashup.js
@@ -65,21 +65,3 @@ require( ["js/qlik"], function ( qlik ) {
 	
 	app.getObject('pie_chart','hVKPhn');
 		
-//DOWNLOAD BY USING SELECT OPTIONS INSPIRED BY https://jsfiddle.net/mindspank/hozs7db4/ PER https://community.qlik.com/t5/Qlik-Sense-Integration-Extensions-APIs/Creating-a-dynamic-exportData-button-re-using-same-button-for/td-p/1284197
-$(document).ready(function() {
-    //export and download starts 
-    $(document).on('click', ".downloaddata", function(e) {
-        var downloaddiv = $('#' + $(this).parent().parent().find('#data-download option:selected').attr('value'))[0];	
-        console.log(downloaddiv);
-        console.log('downloaddata');		
-        var objScope = angular.element(downloaddiv.children[0]).scope();
-        var objId = objScope.model.id;
-        app.getObject(objId).then(function(model) { 
-            var table = qlik.table(model); 
-            table.exportData({
-                //'format':'CSV_C','filename': 'export_limit_is_5million_cells','download': true // exports to csv; strips leading zeroes; for large datasets https://help.qlik.com/en-US/sense-developer/June2020/APIs/EngineAPI/services-GenericObject-ExportData.html
-                'filename': 'chart_data','download': true // exports to excel with a limit of 1 million cells; retains leading zeros but holds less data than csv above
-            }); 
-        });
-     });   
-});
